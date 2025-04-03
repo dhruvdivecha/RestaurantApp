12288f8f993fd3e0435a98d054410774864d187b
Dhruv Divecha <157246271+dhruvdivecha@users.noreply.github.com>
2025-04-04 00:52:32 +0300

    Added mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,16 @@
 import express, { Response, Request} from "express"
 import cors from "cors"
 import "dotenv/config"
+import mongoose from "mongoose"
+
+const uri = process.env.MONGO_URI;
+   if (!uri) {
+     throw new Error('MONGODB_URI is not defined in environment variables');
+   }
+
+mongoose
+.connect(uri as string)
+.then(() => console.log("Connected to DB"))
 
 const app = express();
 const PORT = 4000;
