b727d28275c10ee870f21b8fcf235d0e3a5246cb
Dhruv Divecha <157246271+dhruvdivecha@users.noreply.github.com>
2025-04-12 16:41:08 +0300

    Added build command

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,6 +21,10 @@ app.use(cors())
 
 app.use("/api/my/user", myUserRouter)
 
+app.get("/health", (req: Request, res: Response) => {
+    res.status(200).json({ message: "OK" })
+})
+
 app.listen(PORT, ()=> {
     console.log(`server started on http://localhost:${PORT}`)
 })
