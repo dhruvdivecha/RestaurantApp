f85ef87e2415762441e722fcf02a3f38ad3e4ef6
Dhruv Divecha <157246271+dhruvdivecha@users.noreply.github.com>
2025-04-28 14:21:48 +0300

    fixed logout redirect issue to localhost

--- frontend/src/pages/OrderStatus.tsx
+++ frontend/src/pages/OrderStatus.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+// import React from "react";
 
-const OrderStatus = () => {
-  return <div>OrderStatus</div>;
-};
+// const OrderStatus = () => {
+//   return <div>OrderStatus</div>;
+// };
 
-export default OrderStatus;
+// export default OrderStatus;
