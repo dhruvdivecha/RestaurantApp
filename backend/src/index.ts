1aa03a427274f2a8ded9a875c8a9c764c879015b
Dhruv Divecha <157246271+dhruvdivecha@users.noreply.github.com>
2025-04-29 09:46:10 +0300

    Handled security issues arised by github

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,26 +18,32 @@ mongoose
 const app = express();
 const PORT = 4000;
 
+// strip the dangerous React-Router headers
+app.use((req: Request, res: Response, next) => {
+  delete (req.headers as any)['x-react-router-prerender-data'];
+  delete (req.headers as any)['x-react-router-spa-mode'];
+  next();
+});
 
+// cors
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.use(express.json()) //converts body into json data 
+app.use(express.json());
 
-app.use("/api/my/user", myUserRouter)
-console.log("Registering /api/my/menu route");
-app.use("/api/my/menu", menuItemRouter)
-app.use("/api/my/usermenu", userMenuRouter)
+// routers
+app.use("/api/my/user", myUserRouter);
+app.use("/api/my/menu", menuItemRouter);
+app.use("/api/my/usermenu", userMenuRouter);
 
-app.get("/health", (req: Request, res: Response) => {
-    res.status(200).json({ message: "OK" })
-})
+// Health check & listener
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "OK" });
+});
 
-
-
-app.listen(PORT, ()=> {
-    console.log(`server started on http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server started on http://localhost:${PORT}`);
+});
